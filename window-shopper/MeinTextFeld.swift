cfbb1398e8b96d2af05c9a8cb7147e3e2b5d3a23
michael <michael@mholzinger.at>
2018-11-05 20:20:37 +0100

    erste version erster commit

---
+++ window-shopper/MeinTextFeld.swift
@@ -0,0 +1,39 @@
+//
+//  MeinTextFeld.swift
+//  window-shopper
+//
+//  Created by Michael Holzinger on 05.11.18.
+//  Copyright © 2018 Michael Holzinger. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class MeinTextFeld: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        styling()
+    }
+    
+    override func awakeFromNib() {
+        super .awakeFromNib()
+        
+        styling()
+        
+    }
+    
+    func styling(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        keyboardType = .numberPad
+        textAlignment = .center
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        
+        
+        
+        
+
+    }
+
+}
--- window-shopper/WindowShopperMainVC.swift
+++ window-shopper/WindowShopperMainVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WindowShopperMainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
